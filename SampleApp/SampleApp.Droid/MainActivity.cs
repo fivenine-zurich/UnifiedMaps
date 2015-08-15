69ae7a9bd9692a6126ea3c1e107664ac23873925
toh <tobias.herrmann@fiveninedigital.ch>
2015-08-15 12:36:28 +0200

    Fixed the android sample

---
+++ SampleApp/SampleApp.Droid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.OS;
+using Xamarin.Forms.Platform.Android;
+using Sample;
+using fivenine;
+using Android.Content.PM;
+
+namespace SampleApp.Droid
+{
+	[Activity (Label = "SampleApp.Droid", MainLauncher = true, Icon = "@drawable/icon",
+		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			Xamarin.Forms.Forms.Init (this, bundle);
+			UnifiedMap.Init (this, bundle);
+
+			LoadApplication (new App ());
+		}
+	}
+}
