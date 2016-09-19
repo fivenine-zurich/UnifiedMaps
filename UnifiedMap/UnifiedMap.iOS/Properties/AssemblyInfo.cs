176fac1288a6189d77302cf82f4056da5d4e387f
Tobias Herrmann <tobias.herrmann@fiveninedigital.ch>
2016-09-19 23:54:08 +0200

    removes versioning info from AssemblyInfo files
    +semver:minor

--- UnifiedMap/UnifiedMap.iOS/Properties/AssemblyInfo.cs
+++ UnifiedMap/UnifiedMap.iOS/Properties/AssemblyInfo.cs
@@ -23,17 +23,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("0ef49795-8694-4ddb-a8a9-72ef66e3b450")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- UnifiedMap/UnifiedMap/Properties/AssemblyInfo.cs
+++ UnifiedMap/UnifiedMap/Properties/AssemblyInfo.cs
@@ -16,19 +16,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
 [assembly:InternalsVisibleTo("UnifiedMap.iOS")]
 [assembly: InternalsVisibleTo("UnifiedMap.Windows")]
 [assembly: InternalsVisibleTo("UnifiedMap.Droid")]
